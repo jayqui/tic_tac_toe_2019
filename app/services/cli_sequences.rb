487d8169e5b9d7fe5d2a1bea92236e77f6c18e61
Jay Quigley <jay.quigley@omadahealth.com>
2019-08-17 00:17:31 -0700

    move all putses to a view module

--- app/services/cli_sequences.rb
+++ app/services/cli_sequences.rb
@@ -1,17 +1,33 @@
 module CliSequences
-  def intro_sequence
+  def say_welcome
     puts "Welcome to Tic Tac Toe!"
-    puts "\nWhat is the name for Player X?"
-    print "> "
-    p1_name = gets.chomp
-    puts "Welcome, #{p1_name}!"
+  end
 
-    puts "\nWhat is the name for Player O?"
-    print "> "
-    p2_name = gets.chomp
-    puts "Welcome, #{p2_name}!"
+  def say_draw
+    puts "It's a draw!"
+  end
 
-    puts "\nLet's do this!"
-    puts "\nHere's the board:"
+  def say_winner(name_or_identifier)
+    puts "#{name_or_identifier} wins!"
+  end
+
+  def say_print_board(board)
+    board.
+      map { |square| square.nil? ? "_" : square }.
+      each_slice(3) { |row| puts "\t" + row.join(" ") }
+    puts
+  end
+
+  def say_take_turn(name_or_identifier, board)
+    puts "\n#{name_or_identifier}'s turn!"
+    say_print_board(board)
+
+    puts "name a square for the next piece, 0-8:"
+    print "> "
+  end
+
+  def say_bad_input(error_message)
+    puts error_message
+    print "> "
   end
 end
--- app/tic_tac_toe.rb
+++ app/tic_tac_toe.rb
@@ -1,13 +1,12 @@
 require 'pry'
 
-# TODO: require all in ./services
 require_relative "player.rb"
-# require_relative "services/cli_sequences.rb"
+require_relative "services/cli_sequences.rb"
 require_relative "services/input_validator.rb"
 require_relative "services/win_checker.rb"
 
 class TicTacToe
-  # include CliSequences
+  include CliSequences
 
   attr_reader :p1, :p2
   attr_accessor :board, :input_errors, :whose_turn
@@ -25,15 +24,24 @@ class TicTacToe
   end
 
   def self.intro_sequence
-    puts "Welcome to Tic Tac Toe!"
     new(p1_name: '', p2_name: '').play_game
   end
 
   def play_game
+    say_welcome
+
     while !game_over?
       turn
       self.whose_turn = whose_turn == p1 ? p2 : p1
     end
+    display_end_state
+  end
+
+  def display_end_state
+    say_draw if cats_game?
+    say_winner(p1.piece) if win?(p1.piece)
+    say_winner(p2.piece) if win?(p2.piece)
+    say_print_board(board)
   end
 
   def game_over?
@@ -45,40 +53,28 @@ class TicTacToe
   end
 
   def cats_game?
+    return false if win?(p1.piece) || win?(p2.piece)
     board.each { |square| return false if square.nil? }
     true
   end
 
   def turn
-    puts "\n#{whose_turn.piece}'s turn!"
-    print_board
-
-    puts "name a square for the next piece, 0-8:"
-    print "> "
+    say_take_turn(whose_turn.piece, board)
 
     placement_index = gets.chomp
-
     while invalid_input?(placement_index)
-      input_errors.each_value { |error_message| puts error_message }
-      print "> "
+      input_errors.each_value { |error_message| say_bad_input(error_message) }
       placement_index = gets.chomp
     end
 
-    place(whose_turn.piece, placement_index.to_i)
+    place(placement_index.to_i)
   end
 
   def invalid_input?(placement_index)
     !InputValidator.valid_placement_index?(self, placement_index)
   end
 
-  def print_board
-    board.
-      map { |square| square.nil? ? "_" : square }.
-      each_slice(3) { |row| puts "\t" + row.join(" ") }
-    puts
-  end
-
-  def place(player_piece, board_index)
-    board[board_index] = player_piece
+  def place(board_index)
+    board[board_index] = whose_turn.piece
   end
 end
